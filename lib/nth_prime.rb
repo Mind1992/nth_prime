5da8370d9d77f78f365ea474c995e85283213314
Sergii Mertsalov <sergmerts@gmail.com>
2014-07-28 19:32:58 -0700

    find nth prime number

--- lib/nth_prime.rb
+++ lib/nth_prime.rb
@@ -1,13 +1,11 @@
-def nth_prime(number)
+def nth_prime(number)	
   array_of_numbers = (2..number).to_a
   array_of_numbers.each do |current_number|
     array_of_numbers.reject! do |number|
-      puts current_number
-      puts number
       current_number != number && number % current_number == 0
     end
   end
-  array_of_numbers
+  nth_number =  array_of_numbers.index(number) + 1  
 end
 
-p nth_prime(121)
+
